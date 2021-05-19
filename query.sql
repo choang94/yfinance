8f15b2cba1685555f6e16815c1b6b8d74abd6b2a
Chau (Jo) Hoang <55850536+choang94@users.noreply.github.com>
2021-05-19 13:23:07 -0400

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,15 @@
+SELECT name,
+         SUBSTRING(ts,
+         12,
+         2) AS hour,
+         ROUND(max(high),
+         2) AS highest_price
+FROM findata17
+GROUP BY  SUBSTRING(ts,12,2), name
+ORDER BY  highest_price DESC;
+
+-- additional query --
+
+SELECT name, min(low) AS lowest
+FROM findata17
+GROUP BY name;
