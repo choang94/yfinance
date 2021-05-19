6c42fd0a25a4d4fc00829b7558d9b6ee54db970e
Chau (Jo) Hoang <55850536+choang94@users.noreply.github.com>
2021-05-19 13:22:45 -0400

    Delete query.sql

--- query.sql
+++
@@ -1,9 +0,0 @@
-SELECT name,
-         SUBSTRING(ts,
-         12,
-         2) AS hour,
-         ROUND(max(high),
-         2) AS highest_price
-FROM findata17
-GROUP BY  SUBSTRING(ts,12,2), name
-ORDER BY  highest_price DESC;
